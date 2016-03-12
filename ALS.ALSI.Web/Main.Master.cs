6701731a19eabffee65505dc3b60ad4702f8b580
pawit sae-eaung <pawit1357@hotmail.com>
2016-03-12 23:06:58 +0700

    20160312_2306

--- ALS.ALSI.Web/Main.Master.cs
+++ ALS.ALSI.Web/Main.Master.cs
@@ -72,58 +72,160 @@ namespace ALS.ALSI.Web
                     "<span class=\"label label-success\"><i class=\"icon-plus\"></i></span>" +
                     "{0}.{1}-{2} <span class=\"time\">{3}</span></a></li>";
 
-            //int countBadge = 0;
-            //List<job_sample_logs> logs = new job_sample_logs().SelectNotification();
+            /*
+                    < li class="dropdown dropdown-extended dropdown-notification" id="header_notification_bar">
+                        <a href = "javascript:;" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-close-others="true">
+                            <i class="icon-bell"></i>
+                            <span class="badge badge-default">7 </span>
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li class="external">
+                                <h3>
+                                    <span class="bold">12 pending</span> notifications</h3>
+                                <a href = "page_user_profile_1.html" > view all</a>
+                            </li>
+                            <li>
+                                <ul class="dropdown-menu-list scroller" style="height: 250px;" data-handle-color="#637283">
+                                    <li>
+                                        <a href = "javascript:;" >
+                                            < span class="time">just now</span>
+                                            <span class="details">
+                                                <span class="label label-sm label-icon label-success">
+                                                    <i class="fa fa-plus"></i>
+                                                </span>New user registered. </span>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href = "javascript:;" >
+                                            < span class="time">3 mins</span>
+                                            <span class="details">
+                                                <span class="label label-sm label-icon label-danger">
+                                                    <i class="fa fa-bolt"></i>
+                                                </span>Server #12 overloaded. </span>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href = "javascript:;" >
+                                            < span class="time">10 mins</span>
+                                            <span class="details">
+                                                <span class="label label-sm label-icon label-warning">
+                                                    <i class="fa fa-bell-o"></i>
+                                                </span>Server #2 not responding. </span>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href = "javascript:;" >
+                                            < span class="time">14 hrs</span>
+                                            <span class="details">
+                                                <span class="label label-sm label-icon label-info">
+                                                    <i class="fa fa-bullhorn"></i>
+                                                </span>Application error. </span>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href = "javascript:;" >
+                                            < span class="time">2 days</span>
+                                            <span class="details">
+                                                <span class="label label-sm label-icon label-danger">
+                                                    <i class="fa fa-bolt"></i>
+                                                </span>Database overloaded 68%. </span>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href = "javascript:;" >
+                                            < span class="time">3 days</span>
+                                            <span class="details">
+                                                <span class="label label-sm label-icon label-danger">
+                                                    <i class="fa fa-bolt"></i>
+                                                </span>A user IP blocked. </span>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href = "javascript:;" >
+                                            < span class="time">4 days</span>
+                                            <span class="details">
+                                                <span class="label label-sm label-icon label-warning">
+                                                    <i class="fa fa-bell-o"></i>
+                                                </span>Storage Server #4 not responding dfdfdfd. </span>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href = "javascript:;" >
+                                            < span class="time">5 days</span>
+                                            <span class="details">
+                                                <span class="label label-sm label-icon label-info">
+                                                    <i class="fa fa-bullhorn"></i>
+                                                </span>System Error. </span>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href = "javascript:;" >
+                                            < span class="time">9 days</span>
+                                            <span class="details">
+                                                <span class="label label-sm label-icon label-danger">
+                                                    <i class="fa fa-bolt"></i>
+                                                </span>Storage server failed. </span>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </li>
+                        </u>
+                        */
 
-            //m_status mStatus = new m_status();
-            //m_type_of_test mTypeOfTest = new m_type_of_test();
-            //job_sample jobSample = new job_sample();
-            //foreach (job_sample_logs log in logs)
-            //{
-                //Show only 5 Row
-                //if (countBadge < 5)
-                //{
 
-                //    //2. Show by responsible.
-                //    if (userLogin.responsible_test != null)
-                //    {
-                //        jobSample = jobSample.SelectByID((int)log.job_sample_id);
-                //        if (jobSample != null)
-                //        {
-                //            mTypeOfTest = mTypeOfTest.SelectByID(jobSample.type_of_test_id);
-                //            if (mTypeOfTest != null)
-                //            {
-                //                if (userLogin.responsible_test.StartsWith(mTypeOfTest.name))
-                //                {
-                //                        mStatus = mStatus.SelectByID(log.role_id);
-                //                        if (mStatus != null)
-                //                        {
-                //                            notificationList += String.Format(htmlNotification, (countBadge + 1), log.m_status.name, (String.IsNullOrEmpty(log.job_remark) ? String.Empty : log.job_remark), Convert.ToDateTime(log.date).ToString("MM/dd/yyyy"));
-                //                            countBadge++;
-                //                        }
-                //                }
-                //            }
-                //        }
-                //    }
-                //    else
-                //    {
-                //        //1. Show By Role if not having responsible.
-                //        //mStatus = mStatus.SelectByID(log.role_id);
-                //        //if (mStatus != null)
-                //        //{
-                //        //    if (userLogin.role_id == mStatus.ID)
-                //        //    {
-                //        //        notificationList += String.Format(htmlNotification, (countBadge + 1), log.m_status.name, (String.IsNullOrEmpty(log.job_remark) ? String.Empty : log.job_remark), Convert.ToDateTime(log.date).ToString("MM/dd/yyyy"));
-                //        //        countBadge++;
-                //        //    }
-                //        //}
-                //    }
-                //}
-            //}
 
-            //lbCountBadge.Text = countBadge.ToString();
-            //lbCountBadge_1.Text = lbCountBadge.Text;
+        //int countBadge = 0;
+        //List<job_sample_logs> logs = new job_sample_logs().SelectNotification();
 
-        }
+        //m_status mStatus = new m_status();
+        //m_type_of_test mTypeOfTest = new m_type_of_test();
+        //job_sample jobSample = new job_sample();
+        //foreach (job_sample_logs log in logs)
+        //{
+        //Show only 5 Row
+        //if (countBadge < 5)
+        //{
+
+        //    //2. Show by responsible.
+        //    if (userLogin.responsible_test != null)
+        //    {
+        //        jobSample = jobSample.SelectByID((int)log.job_sample_id);
+        //        if (jobSample != null)
+        //        {
+        //            mTypeOfTest = mTypeOfTest.SelectByID(jobSample.type_of_test_id);
+        //            if (mTypeOfTest != null)
+        //            {
+        //                if (userLogin.responsible_test.StartsWith(mTypeOfTest.name))
+        //                {
+        //                        mStatus = mStatus.SelectByID(log.role_id);
+        //                        if (mStatus != null)
+        //                        {
+        //                            notificationList += String.Format(htmlNotification, (countBadge + 1), log.m_status.name, (String.IsNullOrEmpty(log.job_remark) ? String.Empty : log.job_remark), Convert.ToDateTime(log.date).ToString("MM/dd/yyyy"));
+        //                            countBadge++;
+        //                        }
+        //                }
+        //            }
+        //        }
+        //    }
+        //    else
+        //    {
+        //        //1. Show By Role if not having responsible.
+        //        //mStatus = mStatus.SelectByID(log.role_id);
+        //        //if (mStatus != null)
+        //        //{
+        //        //    if (userLogin.role_id == mStatus.ID)
+        //        //    {
+        //        //        notificationList += String.Format(htmlNotification, (countBadge + 1), log.m_status.name, (String.IsNullOrEmpty(log.job_remark) ? String.Empty : log.job_remark), Convert.ToDateTime(log.date).ToString("MM/dd/yyyy"));
+        //        //        countBadge++;
+        //        //    }
+        //        //}
+        //    }
+        //}
+        //}
+
+        //lbCountBadge.Text = countBadge.ToString();
+        //lbCountBadge_1.Text = lbCountBadge.Text;
+
+    }
     }
 }
